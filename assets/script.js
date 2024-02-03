e268745b4760d0b0733e853bd51a89eab0895826
wellheythereMJ <abetterplace@outlook.com>
2024-02-03 16:16:51 -0700

    working to solve issues

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 //API key 129df2e8b7a598f1885a7aee0dcc44ef
-//uses API key to pull from openweather API //
+//uses API key to pull from openweather API //d
 var weatherURL = 'https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=129df2e8b7a598f1885a7aee0dcc44ef&units=imperial';
 
 fetch(weatherURL)
