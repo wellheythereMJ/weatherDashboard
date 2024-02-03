2b51f174063321bb57d0489671afbe3757d8c637
wellheythereMJ <abetterplace@outlook.com>
2024-02-03 14:47:52 -0700

    added comments

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 //API key 129df2e8b7a598f1885a7aee0dcc44ef
+//uses API key to pull from openweather API //
 var weatherURL = 'https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=129df2e8b7a598f1885a7aee0dcc44ef&units=imperial';
 
 fetch(weatherURL)
