3cf96ef7fa6812bf5e1a7d202fbbaf64cef0b0c1
wellheythereMJ <abetterplace@outlook.com>
2024-02-03 11:48:02 -0700

    started js

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+//API key 129df2e8b7a598f1885a7aee0dcc44ef
+var weatherURL = 'https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=129df2e8b7a598f1885a7aee0dcc44ef&units=imperial';
+
+fetch(weatherURL)
+    .then(function(response){
+      return response.json();
+    } )
+    .then(function(data){
+      console.log(data);
+    })
+
